1b6c6751003be2c2245a9c418d2003af4a28e42f
Simon Cozens <simon@simon-cozens.org>
2022-09-20 16:32:59 +0100

    Add all the files

--- database-to-yaml.py
+++ database-to-yaml.py
@@ -17,10 +17,12 @@ DATE_FORMAT = r"%Y-%m-%d %H:%M:%S"
 # node
 #   -> field_solved_by
 #   -> field_tags
+#   -> field_data_field_file
 
 # comment
 #   -> field_data_comment_body
 #   -> field_revision_comment_body
+#   -> field_data_field_file
 #   cid: ID
 #   nid: node
 #   pid: comment parent
@@ -44,9 +46,23 @@ class Node(Base):
     def body(self):
       return session.scalar(select(NodeBody).where(NodeBody.entity_id == self.nid))
 
+    @property
+    def files(self):
+      res = session.scalars(select(FieldFile).filter_by(entity_id = self.nid, entity_type="node"))
+      fids = [x.field_file_fid for x in res]
+      file_list = session.query(ManagedFile).filter(ManagedFile.fid.in_(fids)).all()
+      return file_list
+
+class ManagedFile(Base):
+    __tablename__ = "file_managed"
+    fid = Column(primary_key=True)
+
 class User(Base):
     __tablename__ = "users"
     uid = Column(primary_key=True)
+
+class FieldFile(Base):
+    __tablename__ = "field_data_field_file"
 
 class NodeBody(Base):
     __tablename__ = "field_data_body"
@@ -64,6 +80,13 @@ class Comment(Base):
     @property
     def body(self):
       return session.scalar(select(CommentBody).where(CommentBody.entity_id == self.cid))
+
+    @property
+    def files(self):
+      res = session.scalars(select(FieldFile).filter_by(entity_id = self.cid, entity_type="comment"))
+      fids = [x.field_file_fid for x in res]
+      file_list = session.query(ManagedFile).filter(ManagedFile.fid.in_(fids)).all()
+      return file_list
 
 class CommentBody(Base):
     __tablename__ = "field_data_comment_body"
@@ -88,11 +111,23 @@ def write_user(user, thing):
   if user.picture:
     thing["author"]["picture"] = user.picture
 
+def write_files(target, files):
+  if not files:
+    return
+
+  target["files"] = [
+    {
+      "filename": f.filename,
+      "uri": f.uri
+    } for f in files
+  ]
+
 def add_nested_comments(c, target):
   comment = {
     "body": c.body.comment_body_value,
     "created": datetime.fromtimestamp(c.created).strftime(DATE_FORMAT)
   }
+  write_files(comment, c.files)
   if c.children:
     comment["children"] = []
 
@@ -101,17 +136,21 @@ def add_nested_comments(c, target):
   for child in c.children:
     add_nested_comments(child, comment["children"])
 
+
 for n in tqdm.tqdm(session.query(Node).all()):
-  filename = "node/%i.md" % n.nid
+  filename = "content/node/%i.md" % n.nid
 
   if not n.body:
     continue
 
   node = {
     "title": n.title,
+    "node_type": n.type,
     "body": n.body.body_value,
     "date": datetime.fromtimestamp(n.created).strftime(DATE_FORMAT)
   }
+
+  write_files(node, n.files)
 
   if n.comment_collection:
     node["comments"] = []
@@ -120,4 +159,5 @@ for n in tqdm.tqdm(session.query(Node).all()):
     if c.pid != 0:
       continue
     add_nested_comments(c, node["comments"])
-  open(filename, "w").write(dump(node, explicit_start=True,explicit_end=True,Dumper=Dumper))
+  yamlnode = dump(node, explicit_start=True,Dumper=Dumper)+"\n---\n"
+  open(filename, "w").write(yamlnode)

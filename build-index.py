111296b15489060efa2a962c649a907c095f69fa
Simon Cozens <simon@simon-cozens.org>
2022-09-21 14:53:18 +0100

    Don’t index HTML

--- build-index.py
+++ build-index.py
@@ -1,6 +1,7 @@
 import glob
 import yaml
 import os
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
@@ -8,32 +9,63 @@ except ImportError:
 from tqdm import tqdm
 
 from elasticsearch import Elasticsearch, helpers
+
 es = Elasticsearch("http://localhost:9200")
 
-try:
-	es.indices.delete(index="typophile")
-except Exception as e:
-	pass
+from io import StringIO
+from html.parser import HTMLParser
+import markdown2
 
-es.indices.create(index="typophile", mappings={
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(md):
+    html = markdown2.markdown(md)
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+try:
+    es.indices.delete(index="typophile")
+except Exception as e:
+    pass
+
+es.indices.create(
+    index="typophile",
+    mappings={
         "properties": {
-            "date": { "type": "date","format": "yyyy-MM-dd HH:mm:ss" },
-        }})
+            "date": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
+        }
+    },
+)
+
 
 def bulk_data():
-	for i in tqdm(glob.glob("content/node/*.md")):
-		nid = int(os.path.basename(i).replace(".md", ""))
-		doc = open(i).read()
-		doc = doc[:-4]
-		doc = doc[4:]
-		doc = yaml.load(doc, Loader=Loader)
-		if "author" in doc:
-			doc["author"] = doc["author"].get("name")
-		yield {
-			"_index": "typophile",
-			"_id": nid,
-			"_source": doc
-		}
+    for i in tqdm(glob.glob("content/node/*.md")):
+        nid = int(os.path.basename(i).replace(".md", ""))
+        doc = open(i).read()
+        doc = doc[:-4]
+        doc = doc[4:]
+        doc = yaml.load(doc, Loader=Loader)
+        if "author" in doc:
+            doc["author"] = doc["author"].get("name")
+        doc["body"] = strip_tags(doc["body"])
+        yield {"_index": "typophile", "_id": nid, "_source": doc}
+
+
 # es.index(index="typophile", id=nid, document=doc)
 response = helpers.bulk(es, bulk_data())
-

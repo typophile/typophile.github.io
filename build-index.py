962425b8ff607984b65e23a08f04bcf031d0be4c
Simon Cozens <simon@simon-cozens.org>
2022-09-21 14:13:01 +0100

    Build index in bulk

--- build-index.py
+++ build-index.py
@@ -7,25 +7,33 @@ except ImportError:
     from yaml import Loader, Dumper
 from tqdm import tqdm
 
-from elasticsearch import Elasticsearch
-es = Elasticsearch()
+from elasticsearch import Elasticsearch, helpers
+es = Elasticsearch("http://localhost:9200")
 
-es.indices.delete(index="typophile")
+try:
+	es.indices.delete(index="typophile")
+except Exception as e:
+	pass
 
 es.indices.create(index="typophile", mappings={
         "properties": {
             "date": { "type": "date","format": "yyyy-MM-dd HH:mm:ss" },
         }})
 
-
-for i in tqdm(glob.glob("content/node/*.md")):
-	nid = int(os.path.basename(i).replace(".md", ""))
-	doc = open(i).read()
-	doc = doc[:-4]
-	doc = doc[4:]
-	doc = yaml.load(doc, Loader=Loader)
-	if "author" in doc:
-		doc["author"] = doc["author"].get("name")
-	es.index(index="typophile", id=nid, document=doc)
-
+def bulk_data():
+	for i in tqdm(glob.glob("content/node/*.md")):
+		nid = int(os.path.basename(i).replace(".md", ""))
+		doc = open(i).read()
+		doc = doc[:-4]
+		doc = doc[4:]
+		doc = yaml.load(doc, Loader=Loader)
+		if "author" in doc:
+			doc["author"] = doc["author"].get("name")
+		yield {
+			"_index": "typophile",
+			"_id": nid,
+			"_source": doc
+		}
+# es.index(index="typophile", id=nid, document=doc)
+response = helpers.bulk(es, bulk_data())
 
